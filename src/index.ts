8ef3a8b93c7784b665d8bb65643b6a4fc8ccbe84
junyatakechi <takechi48j@gmail.com>
2023-10-16 09:14:21 +0900

    パッケージインストール

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
+import pinataSDK from "@pinata/sdk";
+import dotenv from "dotenv";
+dotenv.config();
 
 //
 const main = async function(){
-    console.log("Hello World.");
+    console.log(process.env.API_Key);
+    const pinata = new pinataSDK("", "");
+    // const res = await pinata.testAuthentication()
+    // console.log(res)
 }
 main();
